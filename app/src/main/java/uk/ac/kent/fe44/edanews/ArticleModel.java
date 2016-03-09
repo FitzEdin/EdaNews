797cfb8cb6354239fd7c71e0cc4af7d9786308ec
FitzEdin <fitzroy.edinborough@gmail.com>
2016-03-09 01:09:34 +0000

    added search structure in ArticleModel class..

--- app/src/main/java/uk/ac/kent/fe44/edanews/ArticleModel.java
+++ app/src/main/java/uk/ac/kent/fe44/edanews/ArticleModel.java
@@ -21,7 +21,6 @@ import java.util.ArrayList;
 public class ArticleModel {
 
     private ProgressBar myProgressBar;
-    private OnListUpdateListener listUpdateListener;
     private static ArticleModel ourInstance = new ArticleModel();
 
     private static final int height = 200;
@@ -34,6 +33,21 @@ public class ArticleModel {
     }
 
 
+
+    /*Constructor*/
+    private ArticleModel() {
+    }
+    public static ArticleModel getInstance() {
+        return ourInstance;
+    }
+
+    /* Managing our list of articles */
+    private ArrayList<Article> articleList = new ArrayList<>();
+    public ArrayList<Article> getArticleList() {
+        return articleList;
+    }
+
+
     /** Performing Article List requests
      *  The lists are downloaded in batches of size
      *  and any listener is informed via the interface
@@ -42,7 +56,7 @@ public class ArticleModel {
     private String CLIENT_URL = "http://www.efstratiou.info/projects/newsfeed/getList.php?start=";
     private String RECORD_ID = "record_id", TITLE = "title", DATE = "date",
             SHORT_INFO = "short_info", IMAGE_URL = "image_url";
-    private ArrayList<Article> articleList = new ArrayList<>();
+    private OnListUpdateListener listUpdateListener;
     private int mStart = 0;
     private int size = 20;
 
@@ -187,16 +201,85 @@ public class ArticleModel {
 
 
 
-    /*Constructor*/
-    private ArticleModel() {
-    }
-    public static ArticleModel getInstance() {
-        return ourInstance;
+
+    /** Performing search requests
+     *  The lists are downloaded in batches of size
+     *  and any listener is informed via the interface
+     *  */
+    /*members for performing article list request*/
+    private String SEARCH_URL = "http://www.efstratiou.info/projects/newsfeed/getList.php?titleHas=";
+    private String KEY;
+    private OnSearchListUpdateListener searchListUpdateListener;
+    /* Managing our list of articles */
+    private ArrayList<Article> searchList = new ArrayList<>();
+    public ArrayList<Article> getSearchList() {
+        return searchList;
     }
 
-    public ArrayList<Article> getArticleList() {
-        return articleList;
+    private Response.Listener<JSONArray> searchListener = new Response.Listener<JSONArray>() {
+        @Override
+        public void onResponse(JSONArray response) {
+            //handle response from server
+            searchList.clear();
+            try{
+                for(int i = 0; i < response.length(); i++) {
+                    //extract JSON object from response
+                    JSONObject object = response.getJSONObject(i);
+
+                    //build article with the data
+                    Article article = new Article(
+                            object.getString(IMAGE_URL),
+                            object.getInt(RECORD_ID),
+                            object.getString(TITLE),
+                            object.getString(SHORT_INFO),
+                            object.getString(DATE)
+                    );
+
+                    //and add to list of Articles
+                    getSearchList().add(article);
+                }
+            }catch(JSONException e) {
+                //handle exception
+            }
+            notifySearchListener();
+        }
+    };
+    private Response.ErrorListener searchErrorListener = new Response.ErrorListener() {
+        @Override
+        public void onErrorResponse(VolleyError error) {
+            //handle error in response
+        }
+    };
+    /*searches for a set of articles with a certain title*/
+    public void loadSearchData(ProgressBar progressBar, String key) {
+        //need to handle certain UI elements on this thread
+        myProgressBar = progressBar;
+
+        //build request
+        String url = SEARCH_URL + key;
+        JsonArrayRequest request = new JsonArrayRequest(url, searchListener, searchErrorListener);
+
+        //make request
+        ArticlesApp.getInstance()
+                .getRequestQueue()
+                .add(request);
     }
+    /*let the listener know about updates*/
+    private void notifySearchListener() {
+        if(searchListUpdateListener != null) {
+            myProgressBar.setVisibility(View.INVISIBLE);
+            searchListUpdateListener.onSearchListUpdate();
+        }
+    }
+    /*receive subscriptions for notifications from classes*/
+    public void setOnSearchListUpdateListener(OnSearchListUpdateListener searchListUpdateListener) {
+        this.searchListUpdateListener = searchListUpdateListener;
+    }
+    /*classes that need this data implement this interface*/
+    public interface OnSearchListUpdateListener {
+        void  onSearchListUpdate();
+    }
+    /*end definition for members an methods for performing article list request(s)*/
 
 
 
