c3d2944dcaf05df7739ef8dea5dbe0d5cb9a6685
FitzEdin <fitzroy.edinborough@gmail.com>
2016-03-24 03:36:17 +0000

    gridLayout for SearchList now properly supports both large(>= 480x640), and less than large screens..

--- app/src/main/java/uk/ac/kent/fe44/edanews/SearchListFragment.java
+++ app/src/main/java/uk/ac/kent/fe44/edanews/SearchListFragment.java
@@ -1,5 +1,6 @@
 package uk.ac.kent.fe44.edanews;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -31,10 +32,54 @@ public class SearchListFragment extends ListFragment
         //perform search
         searchFor(key);
 
+
+        int spanCount = 1;
+
+        Configuration config = getResources().getConfiguration();
+
+        //set span count based on screen size and orientation
+        final boolean isLarge = config.isLayoutSizeAtLeast(Configuration.SCREENLAYOUT_SIZE_LARGE);
+        if(isLarge) {
+            //handle large screens, @least 600x1024
+            switch (config.orientation) {
+                case Configuration.ORIENTATION_LANDSCAPE:
+                    spanCount = 3;      //
+                    break;
+                case Configuration.ORIENTATION_PORTRAIT:
+                    spanCount = 2;      //
+                    break;
+            }
+        }else{
+            //handle everything smaller than 600x1024
+            switch (config.orientation) {
+                case Configuration.ORIENTATION_LANDSCAPE:
+                    spanCount = 2;      //
+                    break;
+                case Configuration.ORIENTATION_PORTRAIT:
+                    spanCount = 1;      //
+                    break;
+            }
+        }
+
         //set up layout manager
-        gridLayoutManager = new GridLayoutManager(getActivity(), 2);
+        gridLayoutManager = new GridLayoutManager(getActivity(), spanCount);
         gridLayoutManager.setOrientation(GridLayoutManager.VERTICAL);
         gridLayoutManager.scrollToPosition(0);
+        gridLayoutManager.setSpanSizeLookup(new GridLayoutManager.SpanSizeLookup() {
+            @Override
+            public int getSpanSize(int position) {
+                if (isLarge) {
+                    //for large screens in all orientations
+                    if (((position % 4) == 0) || ((position % 4) == 3)) {
+                        return 2;
+                    } else {
+                        return 1;
+                    }
+                } else {
+                    return 1;
+                }
+            }
+        });
 
         //set up list adapter
         listAdapter = new SearchListAdapter(this);
